20915d388d75e69fce384c046528fdf548e24b6a
ss75710541 <75710541@qq.com>
2022-01-26 15:56:55 +0800

    update docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+set -e
 docker rm -f blockscout
-export NETWORK=Solar
-export SUBNETWORK="SolarNetwork 区域"
-export LOGO_TEXT = "SolarChain-TestNetwork"
-export LOGO=/images/chain-wallet.png
-export LOGO_FOOTER=/images/chain-wallet.png
-export LOGO_FOOTER=/images/ethereum.png
 export ETHEREUM_JSONRPC_VARIANT=geth
 export COIN=ETH
-make start
+export ETHEREUM_JSONRPC_HTTP_URL=http://127.0.0.1:8545
+export DATABASE_URL=postgresql://root:123456@127.0.0.1:5432/blockscout
+
+export NETWORK=SolarNetwork
+export SUBNETWORK="SolarNetwork 区域"
+export LOGO_TEXT="SolarChain-TestNetwork"
+export LOGO=/images/chain-wallet.png
+export LOGO_FOOTER=/images/chain-wallet.png
+export BLOCKSCOUT_VERSION=0.1
+
+DOCKER_IMAGE=quay.io/netwarps/blockscout:pld
+docker pull $DOCKER_IMAGE
+make start -e DOCKER_IMAGE=$DOCKER_IMAGE
