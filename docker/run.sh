0491f0b43bf55b461a749d9ee1583117d82c5598
ss75710541 <75710541@qq.com>
2022-05-19 16:26:43 +0800

    修改品牌信息

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,9 @@ set -e
 docker rm -f blockscout
 export ETHEREUM_JSONRPC_VARIANT=geth
 export COIN=SC
+export COIN_NAME=SC1
 export ETHEREUM_JSONRPC_HTTP_URL=http://172.35.0.239:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://172.35.0.239:8546
 export DATABASE_URL=postgresql://root:123456@127.0.0.1:5432/blockscout
 
 export NETWORK=SolarNetwork
@@ -18,6 +20,12 @@ export LOGO=/images/chain-wallet.png
 export LOGO_FOOTER=/images/chain-wallet.png
 export BLOCKSCOUT_VERSION=0.1
 
-DOCKER_IMAGE=quay.io/netwarps/blockscout
+export DISABLE_EXCHANGE_RATES=true
+export SHOW_PRICE_CHART=true
+
+#export DISABLE_READ_API=true
+#export DISABLE_WRITE_API=true
+
+DOCKER_IMAGE=quay.io/netwarps/blockscout:jyliu
 #docker pull $DOCKER_IMAGE
 make start -e DOCKER_IMAGE=$DOCKER_IMAGE
