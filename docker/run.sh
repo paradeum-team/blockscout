a1378241d7a99a8f6c5710a166b2506a2820c20b
ss75710541 <75710541@qq.com>
2022-05-20 10:19:03 +0800

    调整运行参数

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,7 @@ set -e
 docker rm -f blockscout
 export ETHEREUM_JSONRPC_VARIANT=geth
 export COIN=SC
-export COIN_NAME=SC1
+export COIN_NAME=SC
 export ETHEREUM_JSONRPC_HTTP_URL=http://172.35.0.239:8545
 export ETHEREUM_JSONRPC_WS_URL=ws://172.35.0.239:8546
 export DATABASE_URL=postgresql://root:123456@127.0.0.1:5432/blockscout
@@ -21,11 +21,12 @@ export LOGO_FOOTER=/images/chain-wallet.png
 export BLOCKSCOUT_VERSION=0.1
 
 export DISABLE_EXCHANGE_RATES=true
-export SHOW_PRICE_CHART=true
+export SHOW_PRICE_CHART=false
+export SHOW_TXS_CHART=true
 
-#export DISABLE_READ_API=true
-#export DISABLE_WRITE_API=true
+export DISABLE_READ_API=true
+export DISABLE_WRITE_API=true
 
 DOCKER_IMAGE=quay.io/netwarps/blockscout:jyliu
-#docker pull $DOCKER_IMAGE
+docker pull $DOCKER_IMAGE
 make start -e DOCKER_IMAGE=$DOCKER_IMAGE
